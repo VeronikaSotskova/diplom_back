301116e55905bf1df330c6bd8d145f2ad3ecdf52
VeronikaSotskova <verasos@mail.ru>
2022-03-30 21:38:37 +0300

    Размер круга зависит от количества вложенных таблиц/доменов

--- core/serializers/BusinessDomainsSerializer.py
+++ core/serializers/BusinessDomainsSerializer.py
@@ -15,7 +15,16 @@ class BusinessDomainsSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         instance.has_children = instance.children.exists() or instance.tables.exists()
-        instance.value = 10
+        
+        count_tables = instance.tables.count()
+        nodes_to_check = list(instance.children.all())
+        while len(nodes_to_check) > 0:
+            cur_node = nodes_to_check.pop()
+            children_node = cur_node.children
+            if children_node.exists():
+                nodes_to_check.extend(list(children_node.all()))
+            count_tables += cur_node.tables.count()
+        instance.value = count_tables + 1
         representation = super().to_representation(instance)
         return representation
 
