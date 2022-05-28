192c53f2afe27d09e8084b6a3d4d2db9f66c8222
Veronika <verasos13@gmail.com>
2022-05-28 23:08:33 +0300

    cors

--- src/settings.py
+++ src/settings.py
@@ -103,5 +103,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS settings
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_WHITELIST_ORIGINS = ['http://127.0.0.1', 'http://localhost:3000', 'http://localhost:8080', 'https://diplomfront.herokuapp.com']
-CORS_ORIGIN_REGEX_WHITELIST = ['http://localhost:3000', 'https://localhost:8080', 'https://diplomfront.herokuapp.com']
+CORS_WHITELIST_ORIGINS = ['http://127.0.0.1', 'http://localhost:3000', 'http://localhost:8080', 'https://diploma-front.herokuapp.com']
+CORS_ORIGIN_REGEX_WHITELIST = ['http://localhost:3000', 'https://localhost:8080', 'https://diploma-front.herokuapp.com']
